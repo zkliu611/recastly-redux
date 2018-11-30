4835b753cabf9ceb2e3451869c62ad42d21e2fc6
zkliu611 <zkliu61@gmail.com>
2018-11-29 16:17:47 -0800

    implement debouce

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,7 +5,7 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
-  return (dispatch) => {
+  return _.debounce((dispatch) => {
     var options = {
       query: q,
       key: YOUTUBE_API_KEY
@@ -15,7 +15,7 @@ var handleVideoSearch = (q) => {
         dispatch(changeVideoList(videos));
     };
     searchYouTube(options, callback);
-  }
+  }, 500);
   //TODO:  Write an asynchronous action to handle a video search!
   
 };
